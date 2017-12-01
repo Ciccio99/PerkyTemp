dd657bcf09227bf2f044bad10225c83f8401e380
Alberto Scicali <afs2842@rit.edu>
2017-12-01 10:07:18 -0500

    Added project layout and initial implementation of some components

---
+++ PerkyTemp/Models/PastSession.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace PerkyTemp.Models
+{
+    public class PastSession
+    {
+        public byte[] MAC { get; private set; }
+        public DateTime startDateTime { get; private set; }
+        public DateTime finalDateTime { get; private set; }
+        public double startTemp { get; private set; }
+        public double finalTemp { get; private set; }
+
+
+        public PastSession(byte[] MAC, DateTime startDateTime, DateTime finalDateTime, double startTemp, double finalTemp) {
+            this.MAC = MAC;
+            this.startDateTime = startDateTime;
+            this.finalDateTime = finalDateTime;
+            this.startTemp = startTemp;
+            this.finalTemp = finalTemp;
+        }
+
+        public Int64 GetSessionDuration () {
+            return (finalDateTime - startDateTime).Minutes;
+        }
+    }
+}
---
+++ PerkyTemp/Views/CurrentSessionView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace PerkyTemp.Views
+{
+    public partial class CurrentSessionView : ContentView
+    {
+        public CurrentSessionView()
+        {
+            InitializeComponent();
+        }
+    }
+}

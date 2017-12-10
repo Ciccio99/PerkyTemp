cde8eb9b8d634075a4d9b923f020bea35d64cc5c
Jake Hartz <jake-git@hartz.io>
2017-12-09 20:05:58 -0500

    Works for me now on Android + Win7

---
+++ Droid/Services/BluetoothManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using PerkyTemp.Interfaces;
+using Xamarin.Forms;
+using System.Diagnostics;
+
+[assembly: Dependency(typeof(PerkyTemp.Droid.Services.BluetoothManager))]
+namespace PerkyTemp.Droid.Services
+{
+    public class BluetoothManager : IBluetoothManager
+    {
+        public BluetoothManager()
+        {
+        }
+
+        public string Test()
+        {
+            return "Meow from Android";
+        }
+    }
+}

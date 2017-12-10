51084ceb31b6a83861994b920f0e5a8519da2d0a
Alberto Scicali <afs2842@rit.edu>
2017-12-09 19:27:07 -0500

    Initial buetooth stuff

---
+++ PerkyTemp/ViewModels/CurrentSessionViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using PerkyTemp.Interfaces;
+
+namespace PerkyTemp.ViewModels {
+    public class CurrentSessionViewModel : INotifyPropertyChanged{
+
+        public string Log { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private IBluetoothManager _bluetoothManager;
+
+        public CurrentSessionViewModel () {
+            _bluetoothManager = DependencyService.Get<IBluetoothManager> ();
+
+            Log = _bluetoothManager.Test ();
+        }
+
+    }
+}
---
+++ iOS/Services/BluetoothManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using CoreBluetooth;
+using CoreFoundation;
+using PerkyTemp.Interfaces;
+using Xamarin.Forms;
+using System.Diagnostics;
+
+[assembly: Dependency (typeof (PerkyTemp.iOS.Services.BluetoothManager))]
+namespace PerkyTemp.iOS.Services {
+    public class BluetoothManager : IBluetoothManager {
+
+        private PerkyCBCentralManagerDelegate _managerDel;
+        private CBCentralManager _centralManager;
+
+        public BluetoothManager () {
+            _managerDel = new PerkyCBCentralManagerDelegate ();
+            _centralManager = new CBCentralManager (_managerDel, null);
+            Debug.WriteLine ( "Bluetooth manager IOS constructed");
+        }
+
+        public string Test () {
+            return "Meow";
+        }
+    }
+}
